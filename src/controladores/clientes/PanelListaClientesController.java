7237ad8e6a7274a2c74fa4c5cd0c1bc3ac2e4905
NicoEstebanBorquez <nico.esteban.borquez@gmail.com>
2021-11-28 22:00:44 +0100

    .

---
+++ src/controladores/clientes/PanelListaClientesController.java
@@ -0,0 +1,117 @@
+package controladores.clientes;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import modelos.Clientes;
+import modelos.ClientesDAO;
+import modelos.SessionFactorySingleton;
+import org.hibernate.SessionFactory;
+
+public class PanelListaClientesController implements Initializable  {
+
+    @FXML
+    private TableView<Clientes> tablaClientes;
+
+    @FXML
+    private TableColumn colRef, colNombre, colTelefono, colEmail;
+
+    private ObservableList<Clientes> listaObservable;
+
+    SessionFactory sf = SessionFactorySingleton.getSessionFactory();
+
+    @FXML
+    public void abrirNuevoCliente(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/clientes/PanelNuevoCliente.fxml"));
+        //Cargar el padre
+        Parent padre = loader.load();
+        //VistaInicioController controlador = loader.getController();
+        Scene escena = new Scene(padre);
+        Stage escenario = new Stage();
+        escenario.setScene(escena);
+
+        //Haciendo que la ventana sea modal
+        escenario.initModality(Modality.WINDOW_MODAL);
+        escenario.initOwner(((Node) event.getSource()).getScene().getWindow());
+
+        //Elimina la barra superior con el boton de cerrar 
+        escenario.initStyle(StageStyle.UNDECORATED);
+        escenario.setResizable(false);
+        escenario.setTitle("Nuevo cliente");
+        escenario.show();
+    }
+
+    public void mostrarClientes() {
+        //Tabla
+        listaObservable = FXCollections.observableArrayList();
+        this.colRef.setCellValueFactory(new PropertyValueFactory("idCliente"));
+        this.colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        this.colTelefono.setCellValueFactory(new PropertyValueFactory("telefono"));
+        this.colEmail.setCellValueFactory(new PropertyValueFactory("email"));
+        /*
+        *
+        * Esto tengo que ver como meterlo en ClientesDAO
+        * De modo que al llamar a listar() me devuelve el List con los datos
+        * Lo he metido aquí porque por algún motivo solo puedo usarlo una vez porque se me cierra la sesion
+         */
+        Clientes cliente = null;
+        ClientesDAO clienteDAO = new ClientesDAO();
+        List<Clientes> lista = clienteDAO.listar();
+
+        Iterator iterador = lista.iterator();
+        while (iterador.hasNext()) {
+            cliente = (Clientes) iterador.next();
+            this.listaObservable.add(cliente);
+            this.tablaClientes.setItems(listaObservable);
+        }
+    }
+
+    @FXML
+    public void seleccionar() throws IOException {
+        Clientes clienteSeleccionado = this.tablaClientes.getSelectionModel().getSelectedItem();
+        int seleccionado = clienteSeleccionado.getIdCliente();
+
+        //Se envía el id del cliente seleccionado al controlador PanelInfoClientesController
+        PanelInfoClienteController.clienteSeleccionado = seleccionado;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/clientes/panelInfoCliente.fxml"));
+        //Cargar el padre
+        Parent padre = loader.load();
+        Scene escena = new Scene(padre);
+        Stage escenario = new Stage();
+        escenario.setScene(escena);
+        escenario.initStyle(StageStyle.UNDECORATED);
+        escenario.setResizable(false);
+        escenario.setTitle("Info cliente");
+        escenario.show();
+
+        /*
+            *   Me faltaría ver como hacer para que la ventana Info Clientes fuese modal como la de Nuevo Clientes
+            * XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            * XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            * XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+         */
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.mostrarClientes();
+    }
+}
---
+++ src/modelos/ClientesDAO.java
@@ -0,0 +1,110 @@
+package modelos;
+
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class ClientesDAO {
+    
+     public List listar() {
+        List<Clientes> lista = null;
+        SessionFactory sf = null;
+        Session sesion = null;
+        try {
+            sf = SessionFactorySingleton.getSessionFactory();
+            sesion = sf.openSession();
+
+            Query query = sesion.createQuery("FROM Clientes");
+            lista = query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sesion.close();
+            return lista;
+        }
+    }
+
+    public Clientes obtenerPorId(int clienteSeleccionado) {
+        Clientes cliente = null;
+        SessionFactory sf = null;
+        Session sesion = null;
+        try {
+            sf = SessionFactorySingleton.getSessionFactory();
+            sesion = sf.openSession();
+            Query consulta = sesion.createQuery("FROM Clientes WHERE idCliente = " + clienteSeleccionado);
+            List<Clientes> lista = consulta.list();
+            Iterator iterador = lista.iterator();
+            while (iterador.hasNext()) {
+                cliente = (Clientes) iterador.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sesion.close();
+            return cliente;
+        }
+    }
+
+    public int insertar(Clientes cliente) {
+
+        int confirmacion = 0;
+        SessionFactory sf = null;
+        Session sesion = null;
+        Transaction transaccion = null;
+        try {
+            sf = SessionFactorySingleton.getSessionFactory();
+            sesion = sf.openSession();
+            transaccion = sesion.beginTransaction();
+
+            sesion.save(cliente);
+            transaccion.commit();
+            confirmacion = 1;
+        } catch (Exception e) {
+            transaccion.rollback();
+            e.printStackTrace();
+        } finally {
+            sesion.close();
+            return confirmacion;
+        }
+    }
+
+    public int actualizar(Clientes clienteSeleccionado) {
+        int confirmacion = 0;
+        Clientes clienteActualizar = null;
+        SessionFactory sf = null;
+        Session sesion = null;
+        Transaction transaccion = null;
+        try {
+            sf = SessionFactorySingleton.getSessionFactory();
+            sesion = sf.openSession();
+            transaccion = sesion.beginTransaction();
+
+            //Se hace una consulta a la BD con "obtenerPorId", con lo que se obtiene de la BD
+            //el cliente que queremos modificar
+            clienteActualizar = this.obtenerPorId(clienteSeleccionado.getIdCliente());
+            // Ahora se mofican los campos:
+            clienteActualizar.setNombre(clienteSeleccionado.getNombre());
+            clienteActualizar.setApellidos(clienteSeleccionado.getApellidos());
+            clienteActualizar.setDni(clienteSeleccionado.getDni());
+            clienteActualizar.setTelefono(clienteSeleccionado.getTelefono());
+            clienteActualizar.setEmail(clienteSeleccionado.getEmail());
+            clienteActualizar.setDireccion(clienteSeleccionado.getDireccion());
+            clienteActualizar.setIdUsuario(clienteSeleccionado.getIdUsuario());
+          
+            //Se hace el update
+            sesion.update(clienteActualizar);
+            transaccion.commit();
+            confirmacion = 1;
+        } catch (Exception e) {
+            transaccion.rollback();
+            e.printStackTrace();
+        } finally {
+            sesion.close();
+            return confirmacion;
+        }
+    }
+
+}
